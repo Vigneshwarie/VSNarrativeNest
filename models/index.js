6aebe452b77bee4bf651e9c0e9fb519f43e40195
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-12-27 20:21:22 -0500

    Added index file for model

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const BlogComments = require('./BlogComments');
+
+User.hasMany(Blog, {
+     foreignKey: 'user_id',
+});
+
+BlogComments.hasMany(Blog, {
+     foreignKey: 'blog_id',
+});
+
+BlogComments.hasMany(User, {
+     foreignKey: 'user_id',
+});
+
+
+module.exports = {User, Blog, BlogComments}
